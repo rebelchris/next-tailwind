f9c399d8a5d92c5308bc00724adee40a3cb69c3a
Chris Bongers <chrisbongers@gmail.com>
2021-09-22 17:42:56 +0200

    Added active links

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,25 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 const Header = ({ menuItems: { menuItems } }) => {
+  const router = useRouter();
+
   return (
     <nav className='flex flex-wrap items-center justify-between p-6 bg-blue-500 shadow-lg'>
       <ul className='flex items-center justify-end flex-grow w-full'>
         {menuItems.edges.map((item) => (
           <li key={item.node.path}>
-            <a
-              className='p-4 ml-2 text-white hover:underline'
-              href={item.node.connectedNode.node.slug}
-            >
-              {item.node.label}
-            </a>
+            <Link href={item.node.connectedNode.node.slug}>
+              <a
+                className={`cursor-pointer p-4 ml-2 text-white ${
+                  router.asPath === `/${item.node.connectedNode.node.slug}`
+                    ? 'underline'
+                    : ' hover:underline'
+                }`}
+              >
+                {item.node.label}
+              </a>
+            </Link>
           </li>
         ))}
       </ul>
